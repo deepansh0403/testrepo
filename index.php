c6557f511fb1ee26dd69cfe006ee992606cf31af
deepansh0403 <deepanshubisht330@gmail.com>
2025-02-22 11:21:43 +0530

    new code checking

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Welcome to Github Repo</h1>
     <p>This is a simple HTML page created to demonstrate how to use Github.</p>
+    <p>To access the repository, click <a href="https://github.com/YourUsername/YourRepoName">here</a>.</p>
 </body>
 </html>
